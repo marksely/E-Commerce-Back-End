16b9cef0e8bce7bba5274d06d00135ef1e751965
Mark Selyuzhitskiy <mark.sel9@gmail.com>
2021-10-21 23:49:18 -0400

    completed put,post,delete routes for tag routes and category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,16 +32,47 @@ router.get('/:id', async (req, res) => {
   
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {category_name : req.body.category_name},
+      {where: {id: req.params.id }}
+    );
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+
+    if(!categoryData) {
+      res.status(404).json('No category with this id');
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
